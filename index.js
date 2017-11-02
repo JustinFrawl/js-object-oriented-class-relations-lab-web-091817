72ed155f6b711a5a462dbd2323a2baa5fac25b30
github username <github email address>
2017-11-02 12:00:20 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,61 @@
+store = {drivers: [],passengers: [],trips: []}
+let driverids = 0
+class Driver{
+  constructor(name){
+    this.name = name
+    this.id = ++driverids
+    store.drivers.push(this)
+  }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
+  }
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
+  }
+}
+let passengerIds = 0
+class Passenger{
+  constructor(name){
+    this.name = name
+    this.id = ++passengerIds
+    store.passengers.push(this)
+
+  }
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+    });
+  }
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
+  }
+}
+let tripIds = 0
+class Trip {
+  constructor(driver, passenger) {
+    if(driver){
+    this.driverId = driver.id;
+  }
+  if (passenger){
+    this.passengerId = passenger.id;
+  }
+    this.id = tripIds++;
+    store.trips.push(this);
+  }
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+}
